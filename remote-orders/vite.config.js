0194926cad68488dd556b65145c0530c9f2452e6
faizan <faizan.mysecrets@gmail.com>
2026-04-09 11:32:54 +0530

    Fix remote orders cloud React resolution

--- remote-orders/vite.config.js
+++ remote-orders/vite.config.js
@@ -4,6 +4,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "node_modules/react"),
+      "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
+      "react/jsx-runtime": path.resolve(__dirname, "node_modules/react/jsx-runtime.js"),
+      "react/jsx-dev-runtime": path.resolve(__dirname, "node_modules/react/jsx-dev-runtime.js"),
+    },
+  },
   server: {
     fs: {
       allow: [path.resolve(__dirname, "..")],
